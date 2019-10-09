84571cd025db46f835ad0fd9a59124d3c326b3eb
ClarisseU <clarisseum@gmail.com>
2019-10-09 13:10:52 +0200

    create classes and functions in image

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to= '/')
+    img_name = models.CharField(max_length=60)
+    img_description = models.CharField(max_length=60)
+    
+    def save_image():
+        pass
+        
+    def delete_image():
+        pass
+        
+    def update_image():
+        pass
+    
+    def get_image_by_id(id):
+        pass
+    
+    def search_image(category):
+        pass
+    
+    def filter_by_location(location):
+        pass            
+    
+class Location(models.Model):
+    pass
+class Category(models.Model):  
+    pass      
