8ccfa242a6f1b503fc5b89ea67579f2c82d09297
ClarisseU <clarisseum@gmail.com>
2019-10-09 21:00:58 +0200

    add image test

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
-# Create your tests here.
+class ImageTest(TestCase):
+    '''
+    a class to test the Image and it's instances
+    '''
+    def setUp(self):
+        self.picture = Image(id = '123', image ='', img_name='myPicture', img_description='description')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.picture, Image))
+        
+    def test_save_image(self):
+        '''
+        function to check save method
+        '''
+        self.picture.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)    
