1ef2ce084ef44f71332e505a2211e4ca4f84ba09
ClarisseU <clarisseum@gmail.com>
2019-10-10 22:42:24 +0200

    add some changes

--- photos/urls.py
+++ photos/urls.py
@@ -5,5 +5,4 @@ from django.contrib import admin
 urlpatterns=[
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.welcome, name = 'welcome')
-    url(r'^phtotos/', include(photos.urls))
 ]
