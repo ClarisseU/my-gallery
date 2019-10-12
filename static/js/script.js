73f93e4eee3c4eedeef488514592f9050a85fb6d
ClarisseU <clarisseum@gmail.com>
2019-10-12 17:35:58 +0200

    add some changes

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-$('body').on('mouseenter mouseleave', '.dropdown', function(e) {
-    var _d = $(e.target).closest('.dropdown');
-    if (e.type === 'mouseenter') _d.addClass('show');
-    setTimeout(function() {
-        _d.toggleClass('show', _d.is(':hover'));
-        $('[data-toggle="dropdown"]', _d).attr('aria-expanded', _d.is(':hover'));
-    }, 300);
-});
